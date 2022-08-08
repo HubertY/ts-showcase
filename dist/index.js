9a2a2589d5f2c5771a73c1fb3d6611311e5a0ffc
HubertY <yuanhubert@gmail.com>
2022-08-08 15:31:08 -0400

    Fix some pathing issues

--- dist/index.js
+++ dist/index.js
@@ -53,6 +53,9 @@ window._runtimes[${i}] = {};
 }
 export function makeShowcase(inits, domID, localDeps = [], libDir = ".", initialCode = "") {
     return __awaiter(this, void 0, void 0, function* () {
+        if (libDir === "/") {
+            libDir = "";
+        }
         const localLibs = new Map();
         const localScripts = new Map();
         // Create a sandbox and embed it into the the div #monaco-editor-embed
@@ -71,50 +74,54 @@ export function makeShowcase(inits, domID, localDeps = [], libDir = ".", initial
             localLibs.set(path, data);
             const pack = JSON.parse(data);
             if (localDeps.indexOf(pack.name) !== -1) {
-                localScripts.set(pack.name, `./${libDir}/${path.replace("package.json", pack.main)}`);
+                localScripts.set(pack.name, `${libDir}/${path.replace("package.json", pack.main)}`);
             }
         });
         for (const [s, data] of localLibs) {
-            console.log(`/node_modules/${s}`);
+            console.log(`adding /node_modules/${s}`);
             sandbox.addLibraryToRuntime(data, `/node_modules/${s}`);
         }
         return new Showcase(sandbox, localScripts);
     });
 }
+let initialization = undefined;
 export function init(sandboxPath) {
-    return new Promise((resolve, reject) => {
-        console.log("initializing showcase...");
-        // First set up the VSCode loader in a script tag
-        const getLoaderScript = document.createElement("script");
-        getLoaderScript.src = "https://www.typescriptlang.org/js/vs.loader.js";
-        getLoaderScript.async = true;
-        getLoaderScript.onload = () => {
-            // Now the loader is ready, tell require where it can get the version of monaco, and the sandbox
-            // This version uses the latest version of the sandbox, which is used on the TypeScript website
-            // For the monaco version you can use unpkg or the TypeScript web infra CDN
-            // You can see the available releases for TypeScript here:
-            // https://typescript.azureedge.net/indexes/releases.json
-            //
-            window.require.config({
-                paths: {
-                    vs: "https://typescript.azureedge.net/cdn/4.7.3/monaco/min/vs",
-                    sandbox: sandboxPath,
-                },
-                // This is something you need for monaco to work
-                ignoreDuplicateModules: ["vs/editor/editor.main"],
-            });
-            // Grab a copy of monaco, TypeScript and the sandbox
-            window.require(["vs/editor/editor.main", "vs/language/typescript/tsWorker", "sandbox/index"], (editor, _tsWorker, sandbox) => {
-                if (editor && _tsWorker && sandbox) {
-                    console.log("showcase initialized");
-                    resolve({ editor, sandbox: sandbox });
-                }
-                else {
-                    console.log("showcase initialize error");
-                    reject({ editor: !!editor, _tsWorker: !!_tsWorker, sandbox: !!sandbox });
-                }
-            });
-        };
-        document.body.appendChild(getLoaderScript);
-    });
+    if (!initialization) {
+        initialization = new Promise((resolve, reject) => {
+            console.log("initializing showcase...");
+            // First set up the VSCode loader in a script tag
+            const getLoaderScript = document.createElement("script");
+            getLoaderScript.src = "https://www.typescriptlang.org/js/vs.loader.js";
+            getLoaderScript.async = true;
+            getLoaderScript.onload = () => {
+                // Now the loader is ready, tell require where it can get the version of monaco, and the sandbox
+                // This version uses the latest version of the sandbox, which is used on the TypeScript website
+                // For the monaco version you can use unpkg or the TypeScript web infra CDN
+                // You can see the available releases for TypeScript here:
+                // https://typescript.azureedge.net/indexes/releases.json
+                //
+                window.require.config({
+                    paths: {
+                        vs: "https://typescript.azureedge.net/cdn/4.7.3/monaco/min/vs",
+                        sandbox: sandboxPath,
+                    },
+                    // This is something you need for monaco to work
+                    ignoreDuplicateModules: ["vs/editor/editor.main"],
+                });
+                // Grab a copy of monaco, TypeScript and the sandbox
+                window.require(["vs/editor/editor.main", "vs/language/typescript/tsWorker", "sandbox/index"], (editor, _tsWorker, sandbox) => {
+                    if (editor && _tsWorker && sandbox) {
+                        console.log("showcase initialized");
+                        resolve({ editor, sandbox: sandbox });
+                    }
+                    else {
+                        console.log("showcase initialize error");
+                        reject({ editor: !!editor, _tsWorker: !!_tsWorker, sandbox: !!sandbox });
+                    }
+                });
+            };
+            document.body.appendChild(getLoaderScript);
+        });
+    }
+    return initialization;
 }
--- src/index.ts
+++ src/index.ts
@@ -56,6 +56,10 @@ window._runtimes[${i}] = {};
 
 
 export async function makeShowcase(inits: ShowcaseInitialization, domID: string, localDeps: string[] = [], libDir: string = ".", initialCode: string = "") {
+    if (libDir === "/") {
+        libDir = "";
+    }
+
     const localLibs = new Map<string, string>();
     const localScripts = new Map<string, string>();
 
@@ -79,58 +83,61 @@ export async function makeShowcase(inits: ShowcaseInitialization, domID: string,
         localLibs.set(path, data);
         const pack = JSON.parse(data);
         if (localDeps.indexOf(pack.name) !== -1) {
-            localScripts.set(pack.name, `./${libDir}/${path.replace("package.json", pack.main)}`);
+            localScripts.set(pack.name, `${libDir}/${path.replace("package.json", pack.main)}`);
         }
     });
 
     for (const [s, data] of localLibs) {
-        console.log(`/node_modules/${s}`)
+        console.log(`adding /node_modules/${s}`)
         sandbox.addLibraryToRuntime(data, `/node_modules/${s}`);
     }
 
     return new Showcase(sandbox, localScripts);
 }
-
+let initialization: Promise<ShowcaseInitialization> | undefined = undefined;
 export function init(sandboxPath: string): Promise<ShowcaseInitialization> {
-    return new Promise((resolve, reject) => {
-        console.log("initializing showcase...");
-        // First set up the VSCode loader in a script tag
-        const getLoaderScript = document.createElement("script")
-        getLoaderScript.src = "https://www.typescriptlang.org/js/vs.loader.js"
-        getLoaderScript.async = true
-        getLoaderScript.onload = () => {
-            // Now the loader is ready, tell require where it can get the version of monaco, and the sandbox
-            // This version uses the latest version of the sandbox, which is used on the TypeScript website
+    if (!initialization) {
+        initialization = new Promise((resolve, reject) => {
+            console.log("initializing showcase...");
+            // First set up the VSCode loader in a script tag
+            const getLoaderScript = document.createElement("script")
+            getLoaderScript.src = "https://www.typescriptlang.org/js/vs.loader.js"
+            getLoaderScript.async = true
+            getLoaderScript.onload = () => {
+                // Now the loader is ready, tell require where it can get the version of monaco, and the sandbox
+                // This version uses the latest version of the sandbox, which is used on the TypeScript website
 
-            // For the monaco version you can use unpkg or the TypeScript web infra CDN
-            // You can see the available releases for TypeScript here:
-            // https://typescript.azureedge.net/indexes/releases.json
-            //
-            window.require.config({
-                paths: {
-                    vs: "https://typescript.azureedge.net/cdn/4.7.3/monaco/min/vs",
-                    sandbox: sandboxPath,
-                },
-                // This is something you need for monaco to work
-                ignoreDuplicateModules: ["vs/editor/editor.main"],
-            })
+                // For the monaco version you can use unpkg or the TypeScript web infra CDN
+                // You can see the available releases for TypeScript here:
+                // https://typescript.azureedge.net/indexes/releases.json
+                //
+                window.require.config({
+                    paths: {
+                        vs: "https://typescript.azureedge.net/cdn/4.7.3/monaco/min/vs",
+                        sandbox: sandboxPath,
+                    },
+                    // This is something you need for monaco to work
+                    ignoreDuplicateModules: ["vs/editor/editor.main"],
+                })
 
-            // Grab a copy of monaco, TypeScript and the sandbox
-            window.require(["vs/editor/editor.main", "vs/language/typescript/tsWorker", "sandbox/index"], (
-                editor: Monaco,
-                _tsWorker: any,
-                sandbox: SandboxLib
-            ) => {
-                if (editor && _tsWorker && sandbox) {
-                    console.log("showcase initialized");
-                    resolve({ editor, sandbox: sandbox });
-                }
-                else {
-                    console.log("showcase initialize error");
-                    reject({ editor: !!editor, _tsWorker: !!_tsWorker, sandbox: !!sandbox });
-                }
-            })
-        }
-        document.body.appendChild(getLoaderScript)
-    });
+                // Grab a copy of monaco, TypeScript and the sandbox
+                window.require(["vs/editor/editor.main", "vs/language/typescript/tsWorker", "sandbox/index"], (
+                    editor: Monaco,
+                    _tsWorker: any,
+                    sandbox: SandboxLib
+                ) => {
+                    if (editor && _tsWorker && sandbox) {
+                        console.log("showcase initialized");
+                        resolve({ editor, sandbox: sandbox });
+                    }
+                    else {
+                        console.log("showcase initialize error");
+                        reject({ editor: !!editor, _tsWorker: !!_tsWorker, sandbox: !!sandbox });
+                    }
+                })
+            }
+            document.body.appendChild(getLoaderScript)
+        });
+    }
+    return initialization;
 }
